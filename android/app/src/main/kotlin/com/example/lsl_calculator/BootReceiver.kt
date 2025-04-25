8574786e38cfffc2f9c6524d631ff4cf6c0b88ab
pydev <rasuljon8218@gmail.com>
2025-04-25 14:08:49 +0500

    icon

---
+++ android/app/src/main/kotlin/com/example/lsl_calculator/BootReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.lsl_calculator
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            Toast.makeText(context, "Boot detected: launching calculator", Toast.LENGTH_SHORT).show()
+
+            val launchIntent = Intent(context, MainActivity::class.java)
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(launchIntent)
+        }
+    }
+}
--- android/app/src/main/kotlin/com/example/lsl_calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lsl_calculator/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.example.lsl_calculator
 
+import android.os.Bundle
+import android.view.View
 import io.flutter.embedding.android.FlutterActivity
+import android.app.ActivityManager
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        )
+
+        startLockTask() // Start Android kiosk LockTask mode
+    }
+}
